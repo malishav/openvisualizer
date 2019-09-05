5c0a13024447eb93a8209ddba57c3b538491481c
Mališa Vučinić <malishav@gmail.com>
2019-09-05 17:06:57 +0200

    Add a simple script to launch OV multiple times.

---
+++ benchmark_ov.py
@@ -0,0 +1,34 @@
+import argparse
+import subprocess
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('--numRuns',
+        dest       = 'numRuns',
+        type       = int,
+        default    = 1,
+        required   = False,
+        action     = 'store',
+        )
+
+parser.add_argument('--testbed',
+        dest       = 'testbed',
+        choices    = ['iotlab', 'wilab', 'opentestbed'],
+        required   = True,
+        action     = 'store',
+        )
+
+parser.add_argument('--scenario',
+        dest       = 'scenario',
+        choices    = ['industrial-monitoring', 'home-automation', 'building-automation', 'demo-scenario'],
+        required   = True,
+        action     = 'store',
+        )
+
+args = parser.parse_args()
+
+for i in range(args.numRuns):
+    subprocess.call(
+             ['scons', 'runweb', '--port=8082', '--testbed={0}'.format(args.testbed), '--benchmark={0}'.format(args.scenario), '--bootloadTestbed', '--opentun-null'],
+#            ['scons', 'runweb', '--port=8082', '--sim', '--simCount=4', '--simTopology=linear', '--benchmark={0}'.format(args.scenario), '--opentun-null'],
+            )
