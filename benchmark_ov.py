c4ac277f01c56d0c675da65de53934de08b724e4
Mališa Vučinić <malishav@gmail.com>
2019-09-11 17:10:58 +0200

    Add --port option to benchmark script to start at a random port.

--- benchmark_ov.py
+++ benchmark_ov.py
@@ -1,6 +1,7 @@
 import argparse
 import subprocess
 import os
+import random
 
 parser = argparse.ArgumentParser()
 
@@ -26,11 +27,25 @@ parser.add_argument('--scenario',
         action     = 'store',
         )
 
+parser.add_argument('--port',
+        dest       = 'port',
+        type       = int,
+        default    = 0,
+        required   = False,
+        action     = 'store',
+        )
+
 args = parser.parse_args()
+
+# Start at random port if --port is not provided. This allows multiple instances of OV to run on the same machine.
+if args.port == 0:
+        port = random.randint(49152, 65535)
+else:
+        port = args.port
 
 for i in range(args.numRuns):
     subprocess.call(
-             ['scons', 'runweb', '--port=8082', '--testbed={0}'.format(args.testbed), '--benchmark={0}'.format(args.scenario), '--bootloadTestbed', '--opentun-null'],
+             ['scons', 'runweb', '--port={0}'.format(port), '--testbed={0}'.format(args.testbed), '--benchmark={0}'.format(args.scenario), '--bootloadTestbed', '--opentun-null'],
 #            ['scons', 'runweb', '--port=8082', '--sim', '--simCount=4', '--simTopology=linear', '--benchmark={0}'.format(args.scenario), '--opentun-null'],
             )
     # rename OV log to match the run number
